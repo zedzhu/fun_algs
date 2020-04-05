2db3156bbd16bd73aae5c151e666ecc38540d708
zedzhu <zedzhu@outlook.com>
2020-04-05 15:37:01 +0800

    dev

--- integer_factorization.h
+++ integer_factorization.h
@@ -1,20 +1,23 @@
 #include <cstdio>
+#include <vector>
 
-void print_integer_prime_factors(int n)
-{
+using namespace std;
+
+vector<int> calIntegerPrimeFactors(int n) {
+    vector<int> ret;
     int z = 2;
-    while (z * z <= n)
-    {
-        if (n % z == 0)
-        {
-            printf("%d\n", z);
+    while (z * z <= n) {
+        if (n % z == 0) {
+            ret.push_back(z);
             n /= z;
         }
         else
             z++;
     }
 
+    //如果最后n>1，这说明n本身也是素数了。
     if (n > 1)
-        printf("%d\n", n);
+        ret.push_back(n);
+    
+    return ret;
 }
-
